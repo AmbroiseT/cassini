f72034082b33a13c37d90f63848069721890381e
THOMINE <ambroise.thomine@capgemini.com>
2016-03-05 23:06:01 +0100

    Improvement of data storage (ordered Tree)

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 #First attempt at parsing Raw XML from OSRM
 #Performances tests on finding element by id
-
+from random import shuffle
 from structure import NodeElt
+from structure import ElementsABR
 import time
 
 import xml.etree.ElementTree
@@ -11,12 +12,15 @@ e = xml.etree.ElementTree.parse("data/map.osm")
 #The most basic, and less optimised way to store data : a list of all elements
 nodes = []
 
+
+
 for atype in e.findall("node"):
-    nodes.append(NodeElt(atype.get("id"), atype.get("lat"), atype.get("lon")))
+    nodes.append(NodeElt(int(atype.get("id")), atype.get("lat"), atype.get("lon")))
 
 print(len(nodes))
 
-#measuring time to find elements... It's not very efficient with a list, but we'll improve that!
+
+#measuring time to find elements... It's not very efficient  with a list, but we'll improve that! o(N)
 start = time.clock()
 
 match = [node for node in nodes if node.id == 3457435942]
@@ -25,3 +29,23 @@ end = time.clock()
 
 print("Element found in "+str(end-start)+" seconds")
 print("Finding all elts would take "+str((end-start) * len(nodes))+" seconds")
+
+#Other way to store data a ordonnated tree : 
+tree = ElementsABR()
+
+#Shuffle the order of nodes, else our tree will just be a chained list => worse perfomance possible...
+shuffle(nodes)
+for node in nodes:
+    tree.addElementIter(node)
+
+#measuring time to find elements... With a orderred tree, it's way faster! o(log N)
+#The only problem is that it takes more time to create the structure, but it's worth it!
+start = time.clock()
+
+match = tree.findById(3457435942)
+
+end = time.clock()
+
+print("Element found in "+str(end-start)+" seconds")
+print("Finding all elts would take "+str((end-start) * len(nodes))+" seconds")
+
--- structure.py
+++ structure.py
@@ -1,3 +1,60 @@
+class ElementsABR:
+    left = None
+    right = None
+    value = None
+    def __init__(self, value=None):
+        self.value = value
+
+    def findById(self, id):
+        if self.value == None:
+            return None
+        if self.value.id == id :
+            return self.value
+        elif self.value.id < id:
+            return None if self.right == None else self.right.findById(id)
+        else:
+            return None if self.left == None else self.left.findById(id)
+    def show(self):
+        if self.value == None:
+            return
+        if(self.left != None):
+            self.left.show()
+        print(self.value.id)
+        if(self.right != None):
+            self.right.show()
+            
+    def addElement(self, elt):
+        if(self.value == None):
+            self.value = elt
+        elif self.value.id < elt.id:
+            if(self.right == None):
+                self.right = ElementsABR(elt)
+            else:
+                self.right.addElement(elt)
+        else:
+            if(self.left == None):
+                self.left = ElementsABR(elt)
+            else:
+                self.left.addElement(elt)
+    def addElementIter(self, elt):
+        if(self.value == None):
+            self.value = elt
+            return
+        current = self
+        while(True):
+            if current.value.id < elt.id:
+                if(current.right == None):
+                    current.right = ElementsABR(elt)
+                    return
+                else:
+                    current = current.right
+            else:
+                if(current.left == None):
+                    current.left = ElementsABR(elt)
+                    return
+                else:
+                    current = current.left
+
 class Elt:
     id = -1
     visible = True
@@ -24,6 +81,3 @@ class Way(Elt):
 class Area(Way):
     def __init__(self, id, visible=True,):
         super(Area, self).__init__(id, visible)
-
-node = NodeElt(1, 234, 56)
-print(node)
