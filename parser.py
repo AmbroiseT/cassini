1f31cbf202041fa93e4e5dca3f7d2ee790f47ba3
THOMINE <ambroise.thomine@free.fr>
2016-03-06 16:25:50 +0100

    Looks like python standard dictionnary structure is even faster than my ordered Tree! We'll go with that then.

--- parser.py
+++ parser.py
@@ -12,10 +12,13 @@ e = xml.etree.ElementTree.parse("data/map.osm")
 #The most basic, and less optimised way to store data : a list of all elements
 nodes = []
 
+#Standard Python dictionnary, let's see how fast it can be!
+dictnodes = {}
 
 
 for atype in e.findall("node"):
     nodes.append(NodeElt(int(atype.get("id")), atype.get("lat"), atype.get("lon")))
+    dictnodes[int(atype.get("id"))] = NodeElt(int(atype.get("id")), atype.get("lat"), atype.get("lon"))
 
 print(len(nodes))
 
@@ -27,7 +30,17 @@ match = [node for node in nodes if node.id == 3457435942]
 
 end = time.clock()
 
-print("Element found in "+str(end-start)+" seconds")
+print("List : Element found in "+str(end-start)+" seconds")
+print("Finding all elts would take "+str((end-start) * len(nodes))+" seconds")
+
+#measuring time to find elements now with a dictionnary
+start = time.clock()
+
+found = dictnodes[3457435942]
+
+end = time.clock()
+
+print("Dict : Element found in "+str(end-start)+" seconds")
 print("Finding all elts would take "+str((end-start) * len(nodes))+" seconds")
 
 #Other way to store data a ordonnated tree : 
@@ -46,6 +59,6 @@ match = tree.findById(3457435942)
 
 end = time.clock()
 
-print("Element found in "+str(end-start)+" seconds")
+print("Tree : Element found in "+str(end-start)+" seconds")
 print("Finding all elts would take "+str((end-start) * len(nodes))+" seconds")
 
