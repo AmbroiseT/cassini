d3dc84ad3e20071cae168f606beae23a81a774d6
athomine <ambroise.thomine@free.fr>
2016-03-17 21:22:24 +0100

    You can now move around the map with the arrow keys!

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ You are now able to use stylesheets to pick your own choice of colors!
 The format of stylesheets is json, and heavily inspired on css:
 * You can use selectors "key:value" to select an element (key or value can be omitted "key:" or "value:")
 * The rules are cascading : if you have a previous rule for "building:", it will apply (if not overridden) in addition to any rule like "building:hospital"
+
+### Moving around and zooming on the map:
+You are now able to move around the map with the arrow keys of your keyboard, and to zoom with the mousewheel.
+
+It's not really optimised yet, and moving around can be pretty slow, but we'll improve that soon!
--- Tkui.py
+++ Tkui.py
@@ -12,7 +12,7 @@ class TkUI():
         self.echelle = echelle
         self.style = style
         self.top = Tk()
-        self.canvas = canvas = Canvas(self.top, width=self.echelle.maxX, height=self.echelle.maxY)
+        self.canvas = Canvas(self.top, width=self.echelle.maxX, height=self.echelle.maxY)
         self.canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
         self.canvas.bind_all("<Key>", self.on_key_pressed)
 
@@ -29,13 +29,12 @@ class TkUI():
             style_parameters = self.style.get_parameters(way)
             if 2 < len(points) < 50 and way.isArea() and style_parameters.get("visible", False):
                 self.canvas.create_polygon(points, fill=style_parameters.get('color', 'black'),
-                                      outline=style_parameters.get('line-color', 'black'),
-                                      width=0.2 * self.echelle.convert_km_to_px(style_parameters.get('width', 0)))
+                                           outline=style_parameters.get('line-color', 'black'),
+                                           width=0.2 * self.echelle.convert_km_to_px(style_parameters.get('width', 0)))
             else:
                 for i in range(len(points) - 1):
                     self.canvas.create_line(points[i], points[i + 1], fill=style_parameters.get('line-color', 'grey'),
-                                       width=self.echelle.convert_km_to_px(style_parameters.get('width', 0)))
-
+                                            width=self.echelle.convert_km_to_px(style_parameters.get('width', 0)))
 
     def on_mouse_wheel(self, event):
         if event.delta > 0:
@@ -46,27 +45,27 @@ class TkUI():
         self.canvas.pack()
 
     def on_key_pressed(self, event):
-        decalage = 20
+        decal = 20
         if event.keysym == "Down":
-            self.echelle.corner = (self.echelle.corner[0], self.echelle.corner[1] + self.echelle.convert_px_to_km(decalage))
+            self.echelle.corner = (self.echelle.corner[0], self.echelle.corner[1] + self.echelle.convert_px_to_km(decal))
             self.draw()
             self.canvas.pack()
         elif event.keysym == 'Up':
-            self.echelle.corner = (self.echelle.corner[0], self.echelle.corner[1] - self.echelle.convert_px_to_km(decalage))
+            self.echelle.corner = (self.echelle.corner[0], self.echelle.corner[1] - self.echelle.convert_px_to_km(decal))
             self.draw()
             self.canvas.pack()
         elif event.keysym == 'Right':
-            self.echelle.corner = (self.echelle.corner[0] - self.echelle.convert_px_to_km(decalage), self.echelle.corner[1])
+            self.echelle.corner = (self.echelle.corner[0] - self.echelle.convert_px_to_km(decal), self.echelle.corner[1])
             self.draw()
             self.canvas.pack()
         elif event.keysym == 'Left':
-            self.echelle.corner = (self.echelle.corner[0] + self.echelle.convert_px_to_km(decalage), self.echelle.corner[1])
+            self.echelle.corner = (self.echelle.corner[0] + self.echelle.convert_px_to_km(decal), self.echelle.corner[1])
             self.draw()
             self.canvas.pack()
 
 
 if __name__ == '__main__':
-    mapData = create_map_from_file("data/map3.osm")
+    mapData = create_map_from_file("data/map.osm")
 
     mapData.describe()
     echelle = Echelle(mapData, maxX=500)
