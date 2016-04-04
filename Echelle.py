4f2a1135be05cb5d075f0036df43c95799fb8fd3
athomine <ambroise.thomine@free.fr>
2016-04-04 20:25:56 +0200

    Improvement of the code

--- Echelle.py
+++ Echelle.py
@@ -3,35 +3,19 @@ from structure import ParsedMap, Way
 
 
 class Echelle:
-    map = None
     # Facteur multiplicatif pour faire une projection des coords
     mult = 40000 / 360
-    # Facteur correctif pour la longitude(a calculer en fonction de la latitude)
-    corr = 1
-
-    maxX = 500
-    maxY = 500
-
-    # Attributes for zooming and moving :
-    zoom = 1
-    # Relative position in km
-    corner = (0, 0)
-
-    factor_km_px = 1
-
-    width_km = 0
-    height_km = 0
 
     def __init__(self, map, maxX=500):
         assert isinstance(map, ParsedMap)
         self.maxX = maxX
         self.map = map
+        self.zoom = 1
         self.corr = math.cos(math.radians(self.map.max_lat))
         self.height_km = self.convert_lat_to_km(self.map.max_lat - self.map.min_lat)
         self.width_km = self.convert_lon_to_km(self.map.max_lon - self.map.min_lon)
         self.factor_km_px = self.maxX / self.width_km
         self.maxY = self.convert_km_to_px(self.height_km)
-        self.zoom = 1
         self.corner = (0, 0)
 
     # Distance converters
